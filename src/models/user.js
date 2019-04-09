56fbdab87fa354e3ba1d7becb5fca1169e1532c0
nagaraja-sv <venkatanagarajas@gmail.com>
2019-04-09 10:48:20 +0530

    latest changes

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+
+const Task = require('./task')
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -91,6 +94,15 @@ userSchema.pre('save', async function (next) {
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8)
     }
+    next()
+
+})
+
+// delete user tasks when user  is removed 
+
+userSchema.pre('remove', async function (next) {
+    const user = this
+    await Task.deleteMany({owner:user._id})
     next()
 
 })
